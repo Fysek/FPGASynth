b0f99acbd200d7f1ce914041144d4047d57e4a80
Mateusz Dyrdół <mateuszdyrdol@gmail.com>
2017-10-31 00:09:25 +0100

    first

---
+++ synthesizer.v
@@ -0,0 +1,14 @@
+module synthesizer (
+
+	input logic clk,
+	input logic uart_rx,
+	output logic [2:0] datap,
+	output logic [2:0] datan,
+	output logic clkp,
+	output logic clkn
+
+)
+;
+
+
+endmodule
